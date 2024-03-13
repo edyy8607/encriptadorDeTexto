d5aac8e812a799d8266666feccd22e11011a4dc7
edyy8607 <edyy8607@gmail.com>
2024-03-13 09:35:40 -0500

    Termino de hacer la funcionalidad del boton desencriptador y coloco el icono favicon

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function encriptar() {
   let texto = document.getElementById("texto").value;
   let tituloMensaje = document.getElementById("titulo-mensaje");
   let parrafo = document.getElementById("parrafo");
-  let muheco = document.getElementById("muheco");/*no esta obteniendo la imagen 
-   del muheco encriptado*/
+  let muheco = document.getElementById("muheco"); /*no esta obteniendo la imagen del muheco encriptado*/
 
   let textoCifrado = texto
     .replace(/e/gi, "enter")
@@ -19,9 +18,34 @@ function encriptar() {
     muheco.src = "./img/encriptado.jpg"; // Asegúrate de que la ruta sea correcta
   } else {
     muheco.src = "./img/muñeco.png";
-
-    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     tituloMensaje.textContent = "Ningún mensaje encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     alert("Debes escribir algun texto para poder cifrarlo.");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muheco = document.getElementById("muheco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muheco.src = "./img/desencriptado.jpg";
+  } else {
+    muheco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes escribir algún texto");
   }
 }
